80558eb4cc765758022005284ff438643d383a8a
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-17 10:36:10 -0400

    commit tree 8f6c6dd75c3b78a171def6038a506f478e6205ef

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(usersName, usersEmail){
+    const formData = {
+        name: usersName,
+        email: usersEmail
+      };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+      
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        let p = document.createElement('p');
+        p.textContent = object.id;
+        document.querySelector('body').appendChild(p);
+    })
+    .catch(function(error) {
+        let p = document.createElement('p');
+        p.textContent = error.message;
+        document.querySelector('body').appendChild(p);
+    })
+}
